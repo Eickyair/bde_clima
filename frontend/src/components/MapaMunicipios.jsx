81e79d2776edc926b8bd51bbb61525aea9785b33
Eickyair <martinezerickaguilar@comunidad.unam.mx>
2024-11-18 17:15:30 -0600

    cache municipios

--- frontend/src/components/MapaMunicipios.jsx
+++ frontend/src/components/MapaMunicipios.jsx
@@ -6,77 +6,99 @@ import { useMapas } from '../context/Mapas';
 import { useApi } from '../hooks/useApi';
 import { Skeleton } from 'primereact/skeleton';
 import { useGeoJSON } from '../context/GeoJSON';
+
 export const MapaMunicipios = () => {
     const h = 400;
     const [geoData, setGeoData] = useState(null);
-    const [center, setCenter] = useState([19.432794095377233, -99.13145749369393]);
-    const [bounds, setBounds] = useState(null);
+    const [mapView, setMapView] = useState({
+        center: [19.432794095377233, -99.13145749369393],
+        bounds: null
+    });
     const VITE_HOST = import.meta.env.VITE_API_HOST;
     const { addGeoJSON, getGeoJSON } = useGeoJSON();
     const { fecha, idEstado, metricaTmp, setIdMunicipio } = useMapas();
     const { data: res, isLoading: isLoadingMunicipios } = useApi('estados/tmps', { fecha: fecha.toISOString(), id_estado: idEstado });
-    useEffect(() => {
-        const cache = getGeoJSON(idEstado);
-        if (cache) {
-            setGeoData(cache);
-            let centro = turf.centerOfMass(cache);
-            setCenter([centro.geometry.coordinates[1], centro.geometry.coordinates[0]]);
-            const bbox = turf.bbox(cache);
-            setBounds([
-                [bbox[1], bbox[0]],
-                [bbox[3], bbox[2]]
-            ]);
-            return;
-        }
 
-        fetch(`${VITE_HOST}/municipios/${idEstado}.json`).then(res => res.json())
-            .then(data => {
-                setGeoData(data);
-                let centro = turf.centerOfMass(data);
-                setCenter([centro.geometry.coordinates[1], centro.geometry.coordinates[0]]);
-                const bbox = turf.bbox(data);
-                setBounds([
+    useEffect(() => {
+        const fetchData = async () => {
+            setGeoData(null);
+            setMapView({
+                center: [19.432794095377233, -99.13145749369393],
+                bounds: null
+            });
+
+            const cache = getGeoJSON(idEstado);
+            if (cache) {
+                setGeoData(cache);
+                const centro = turf.centerOfMass(cache);
+                const center = [centro.geometry.coordinates[1], centro.geometry.coordinates[0]];
+                const bbox = turf.bbox(cache);
+                const bounds = [
                     [bbox[1], bbox[0]],
                     [bbox[3], bbox[2]]
-                ]);
-                addGeoJSON(idEstado, data);
-            })
-    }, [idEstado])
+                ];
+                setMapView({ center, bounds });
+                return;
+            }
+
+            const response = await fetch(`${VITE_HOST}/municipios/${idEstado}.json`);
+            const data = await response.json();
+            setGeoData(data);
+            addGeoJSON(idEstado, data);
+
+            const centro = turf.centerOfMass(data);
+            const center = [centro.geometry.coordinates[1], centro.geometry.coordinates[0]];
+            const bbox = turf.bbox(data);
+            const bounds = [
+                [bbox[1], bbox[0]],
+                [bbox[3], bbox[2]]
+            ];
+            setMapView({ center, bounds });
+        };
+
+        fetchData();
+    }, [idEstado]);
+
     const buscarMunicipio = (id_municipio) => {
         const municipio = res.data.find(m => m.id_municipio === id_municipio);
         return municipio;
-    }
+    };
+
     const obtenerValor = (municipio) => {
         if (metricaTmp === 'min') {
-            return parseFloat(municipio.tmin)
+            return parseFloat(municipio.tmin);
         }
-        return parseFloat(municipio.tmax)
-    }
+        return parseFloat(municipio.tmax);
+    };
+
     if (isLoadingMunicipios || !res) {
         return <Skeleton width='100%' height={h} />;
     }
+
     let dataMunicipios = res.data;
     let maximo_tmp = dataMunicipios.reduce((max, municipio) => {
-        let tmp = parseFloat(municipio.tmax)
+        let tmp = parseFloat(municipio.tmax);
         return tmp > max ? tmp : max;
     }, 0);
     let minimo_tmp = dataMunicipios.reduce((min, municipio) => {
-        let tmp = parseFloat(municipio.tmin)
+        let tmp = parseFloat(municipio.tmin);
         return tmp < min ? tmp : min;
     }, Infinity);
-    const calcularColorCoolWarm = (id_municipio, maximo, minimo) => {
-        const estado = buscarMunicipio(id_municipio)
-        if (!estado) {
-            return '#fff'
-        }
-        let tmp = obtenerValor(estado)
 
-        let tmp_estandar = (tmp - minimo) / (maximo - minimo)
-        if (metricaTmp === 'min') {
-            return `rgb(0,0,${255 * tmp_estandar})`
+    const calcularColorCoolWarm = (id_municipio, maximo, minimo) => {
+        const estado = buscarMunicipio(id_municipio);
+        if (!estado) {
+            return '#fff';
         }
-        return `rgb(${255 * tmp_estandar},0,0)`
-    }
+        let tmp = obtenerValor(estado);
+
+        let tmp_estandar = (tmp - minimo) / (maximo - minimo);
+        if (metricaTmp === 'min') {
+            return `rgb(0,0,${255 * tmp_estandar})`;
+        }
+        return `rgb(${255 * tmp_estandar},0,0)`;
+    };
+
     const style = (id_municipio) => {
         return {
             fillColor: calcularColorCoolWarm(id_municipio, maximo_tmp, minimo_tmp),
@@ -87,28 +109,36 @@ export const MapaMunicipios = () => {
             fillOpacity: 0.6
         };
     };
+
     return (
         <MapContainer
-            center={center}
+            center={mapView.center}
             zoomControl={false}
-            bounds={bounds}
+            bounds={mapView.bounds}
             style={{ height: h, width: '100%' }}
         >
             <TileLayer url="https://{s}.basemaps.cartocdn.com/rastertiles/dark_nolabels/{z}/{x}/{y}.png" />
-            {geoData && geoData.features.map((f, i) => {
-                return <GeoJSON key={uuidv4()} style={() => style(f.properties.id_municipio)} data={f} onEachFeature={(feature, layer) => {
-                    layer.on({
-                        click: () => {
-                            setIdMunicipio(feature.properties.id_municipio);
-                        }
-                    });
-                    let municipio = buscarMunicipio(feature.properties.id_municipio);
-                    if (!municipio) {
-                        return;
-                    }
-                    let contenido = `<h2>${municipio.nombre}</h2> <br> Máxima: ${municipio.tmax} <br> Mínima: ${municipio.tmin}`
-                    layer.bindPopup(contenido);
-                }} />
+            {geoData && geoData.features.map((f) => {
+                return (
+                    <GeoJSON
+                        key={uuidv4()}
+                        style={() => style(f.properties.id_municipio)}
+                        data={f}
+                        onEachFeature={(feature, layer) => {
+                            layer.on({
+                                click: () => {
+                                    setIdMunicipio(feature.properties.id_municipio);
+                                }
+                            });
+                            let municipio = buscarMunicipio(feature.properties.id_municipio);
+                            if (!municipio) {
+                                return;
+                            }
+                            let contenido = `<h2>${municipio.nombre}</h2> <br> Máxima: ${municipio.tmax} <br> Mínima: ${municipio.tmin}`;
+                            layer.bindPopup(contenido);
+                        }}
+                    />
+                );
             })}
         </MapContainer>
     );
